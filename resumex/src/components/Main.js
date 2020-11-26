6828f1db6f17a59d351c684af7465fdc7570e746
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-11-25 22:09:13 -0700

    trying to fix the table

--- resumex/src/components/Main.js
+++ resumex/src/components/Main.js
@@ -1,143 +1,133 @@
 import React, { useState } from "react";
 
-
 import "./Main.css";
 import useKeyWords from "../Hooks/KeyWords";
 import useSoftSkills from "../Hooks/SoftSkills";
 import pairMatch from "../Helpers/pairMatch";
 import getScores from "../Helpers/getScores";
-import compareText from "../Helpers/compareText"
-import {extractWordsOnly} from "../Helpers/extrahelp";
+import compareText from "../Helpers/compareText";
+import { extractWordsOnly } from "../Helpers/extrahelp";
 import mammoth from "mammoth";
 import DonutWithText from "../components/DonutWithText";
 import BarChart from "../components/BarChart";
 import document_logo from "../assets/img/document_logo.svg";
 import { findAll } from "highlight-words-core";
-import Table from "../components/Table";
+import ResultTable from "./ResultTable";
 import { first } from "underscore";
-
+import repeatCount from "../Helpers/repeatCount";
 
 export default function Main() {
-const [jobPosting, setJobposting] = useState("");
-const [resume, setResume] = useState("");
-const [edit, setEdit] = useState([""]);
-const [PrimaryScore, setPrimaryScore] = useState([""]);
-const [resumeAndPostingComp, setResumeAndPosting] = useState([""]);
-const [highlightWords, setHighlightWords] = useState([""]);
+  const [jobPosting, setJobposting] = useState("");
+  const [resume, setResume] = useState("");
+  const [edit, setEdit] = useState([""]);
+  const [PrimaryScore, setPrimaryScore] = useState([""]);
+  const [resumeAndPostingComp, setResumeAndPosting] = useState([""]);
+  const [highlightWords, setHighlightWords] = useState([""]);
+
+  const { keywords } = useKeyWords();
+  const { softskills } = useSoftSkills();
+
+  //vitalKeywords is an array with the same words of the database and jobposting
+  //for the vital keywords
+
+  const vitalKeywords = pairMatch(keywords, jobPosting);
+  const vitalSoftSkills = pairMatch(softskills, jobPosting);
+  console.log(vitalSoftSkills);
+  
+  //
+  
+  //this a score of how many of the
+  //vital keywords you have in your resume.
+  const firstScore = getScores(vitalKeywords, resume);
+  const secondScore = getScores(vitalSoftSkills, resume);
+  
+  //resumeAndPosting is an array of the words
+  //that repeat on the posting and repeat on the resume with a count of each
+  
+  // const resumeAndPosting = compareText(resume, jobPosting)
+  
+  // const vitalKeywords = pairMatch(keywords, jobPosting)
+  
+  // const firstScore = getScores(vitalKeywords,resume)
+  
+  // const resumeAndPosting = compareText(resume, jobPosting)
+  const resumeRepeatFromPosting = pairMatch(keywords, resume);
+  const resumeRepeatSoftSkillsPosting = pairMatch(softskills, resume);
+  
+  
+  const textToHighlight = resume;
+  const searchWords = highlightWords;
+  const chunks = findAll({
+    searchWords,
+    textToHighlight,
+  });
+
+  const resumeAndPosting = compareText(resume, jobPosting);
+
+  // console.log("show database keywords: ", keywords, "vitalKeywords: ", vitalKeywords)
+  //printarray
 
 
-const { keywords } = useKeyWords();
-const { softskills } = useSoftSkills();
-
-
-
-//vitalKeywords is an array with the same words of the database and jobposting
-//for the vital keywords
-
-
-
-const vitalKeywords = pairMatch(keywords, jobPosting)
-const vitalSoftSkills = pairMatch(softskills, jobPosting);
-console.log("vitalsoftskills:", vitalSoftSkills);
-
-// 
-
-
-//this a score of how many of the 
-//vital keywords you have in your resume.
-const firstScore = getScores(vitalKeywords,resume);
-const secondScore = getScores(vitalSoftSkills, resume);
-
-//resumeAndPosting is an array of the words 
-//that repeat on the posting and repeat on the resume with a count of each
-
-// const resumeAndPosting = compareText(resume, jobPosting) 
-
-
-// const vitalKeywords = pairMatch(keywords, jobPosting)
-
-// const firstScore = getScores(vitalKeywords,resume)
-
-// const resumeAndPosting = compareText(resume, jobPosting) 
-
-
-const textToHighlight = resume
-const searchWords = highlightWords
-const chunks = findAll({
-  searchWords,
-  textToHighlight
-});
-
-
-
-
-
-
-const resumeAndPosting = compareText(resume, jobPosting) 
-
-console.log("resumeandposting is: ", resumeAndPosting);
-// console.log("show database keywords: ", keywords, "vitalKeywords: ", vitalKeywords)
-//printarray
-
-console.log("FIRST SCORE:", firstScore);
-console.log("SECOND SCORE", secondScore);
-
- 
-  //dummy for the barchart   
-  const hardSkillScore = firstScore;   
-  const softSkillScore =  secondScore;   
-  const specificKeywords = 100;   
-  const skillsSum = hardSkillScore + softSkillScore + specificKeywords;   
-  const totalScore = 300;      
-  //Dummy variables for charts      
-  const match = (skillsSum/totalScore * 100).toFixed(2);   
+  //dummy for the barchart
+  const hardSkillScore = firstScore;
+  const softSkillScore = secondScore;
+  const specificKeywords = 100;
+  const skillsSum = hardSkillScore + softSkillScore + specificKeywords;
+  const totalScore = 300;
+  //Dummy variables for charts
+  const match = ((skillsSum / totalScore) * 100).toFixed(2);
   const unmatch = 100 - match;
   //Dummy variables for charts
 
-const onChange = function(event){
-  setJobposting(event.target.value);
-}
-const onChange1 = function(event){
-  setResume(event.target.value);
-}
-const onClick = function (event) {
-  event.preventDefault(); 
-  //set state on the vitalKeywords, firstScore, resumeandPosting
- setEdit(vitalKeywords)
- setPrimaryScore(hardSkillScore)
- setResumeAndPosting(resumeAndPosting)
- setHighlightWords(extractWordsOnly(vitalKeywords)) 
- // console.log("resumeandposting is: ", resumeAndPosting);
-  // console.log("show database keywords: ", keywords, "vitalKeywords: ", vitalKeywords)
-  // console.log("firstScore",firstScore);
-}
-
-const wordTextResume = function(buffer) {
-  //reads the file from resume and changes it to text
-
-  mammoth.extractRawText({arrayBuffer: buffer})
-  .then(function(result){
-      const text = result.value; // The raw text
-      const messages = result.messages;
-      setResume(text);
-    })
-  .done();
-}
-
-const onChangeResume = function(event){
-  //takes the file and reads the file from buffer of array
   
-  const reader = new FileReader();
-  const fileData = event.target.files[0];
-  reader.onloadend = (event) => {
-  wordTextResume(event.target.result);
-  }; //triggers when the reader stops reading file
-
-   reader.readAsArrayBuffer(fileData);
-}
+//Titles for the table
+const hardSkillTitle = "Hard Skills"
+const softSkillTitle = "Soft Skills"
 
 
 
+  const onChange = function (event) {
+    setJobposting(event.target.value);
+  };
+  const onChange1 = function (event) {
+    setResume(event.target.value);
+  };
+  const onClick = function (event) {
+    event.preventDefault();
+    //set state on the vitalKeywords, firstScore, resumeandPosting
+    setEdit(vitalKeywords, vitalSoftSkills);
+    setPrimaryScore(hardSkillScore);
+    setResumeAndPosting(resumeAndPosting);
+    setHighlightWords(extractWordsOnly(vitalKeywords));
+    // console.log("resumeandposting is: ", resumeAndPosting);
+    // console.log("show database keywords: ", keywords, "vitalKeywords: ", vitalKeywords)
+    // console.log("firstScore",firstScore);
+  };
+
+  const wordTextResume = function (buffer) {
+    //reads the file from resume and changes it to text
+
+    mammoth
+      .extractRawText({ arrayBuffer: buffer })
+      .then(function (result) {
+        const text = result.value; // The raw text
+        const messages = result.messages;
+        setResume(text);
+      })
+      .done();
+  };
+
+  const onChangeResume = function (event) {
+    //takes the file and reads the file from buffer of array
+
+    const reader = new FileReader();
+    const fileData = event.target.files[0];
+    reader.onloadend = (event) => {
+      wordTextResume(event.target.result);
+    }; //triggers when the reader stops reading file
+
+    reader.readAsArrayBuffer(fileData);
+  };
 
   const wordTextJob = function (buffer) {
     //reads the file from job and changes it to text
@@ -163,27 +153,25 @@ const onChangeResume = function(event){
   };
 
   const highlightedText = chunks
-  .map(chunk => {
-    const { end, highlight, start } = chunk;
-    const text = textToHighlight.substr(start, end - start);
-    console.log("text",text)
-    console.log("highlight", highlight)
-    if (highlight) {
-      return `<mark style="background-color: yellow">${text}</mark>`;
-    } else {
-      
-      return text;
-      
-    }
-  })
-  .join("");
+    .map((chunk) => {
+      const { end, highlight, start } = chunk;
+      const text = textToHighlight.substr(start, end - start);
+      if (highlight) {
+        return `<mark style="background-color: yellow">${text}</mark>`;
+      } else {
+        return text;
+      }
+    })
+    .join("");
+
+    console.log("VITAL KEY WORDS:", vitalKeywords);
+    console.log("Repeat from resume", resumeRepeatFromPosting)
+
 
   return (
     <>
       <div class="whole-main">
         <section class="jumbotron text-center">
-          
-
           <div class="container">
             <h1 class="jumbotron-heading">ResumeX : Tech Resume Expert</h1>
           </div>
@@ -191,25 +179,45 @@ const onChangeResume = function(event){
           <div className="logo_container">
             <img src={document_logo}></img>
           </div>
-
         </section>
         <section className="input">
           <form id="main_form">
             <div class="job-area">
-              <textarea className="textarea" name="job_description" placeholder="Paste Your Job Description Here" value={jobPosting} onChange={onChange}></textarea>
+              <textarea
+                className="textarea"
+                name="job_description"
+                placeholder="Paste Your Job Description Here"
+                value={jobPosting}
+                onChange={onChange}
+              ></textarea>
               <form class="job-upload">
-              <input type="file" id="myFile" name="filename" onChange={onChangeJob}></input>
+                <input
+                  type="file"
+                  id="myFile"
+                  name="filename"
+                  onChange={onChangeJob}
+                ></input>
               </form>
             </div>
             <div class="resume-area">
-              <textarea className="textarea" name="resume" placeholder="Paste Your Resume Here" value={resume} onChange={onChange1}></textarea>
+              <textarea
+                className="textarea"
+                name="resume"
+                placeholder="Paste Your Resume Here"
+                value={resume}
+                onChange={onChange1}
+              ></textarea>
               <form class="resume-upload">
-              <input type="file" id="myFile" name="filename" onChange={onChangeResume}></input>
+                <input
+                  type="file"
+                  id="myFile"
+                  name="filename"
+                  onChange={onChangeResume}
+                ></input>
               </form>
             </div>
           </form>
         </section>
-        
 
         <button
           type="submit"
@@ -221,9 +229,9 @@ const onChangeResume = function(event){
           Submit
         </button>
         {/* <h4>{edit}</h4> */}
-        <h4>{resumeAndPostingComp}</h4>
+        {/* <h4>{resumeAndPostingComp}</h4> */}
       </div>
-      <h1 className="overview">Summary</h1>
+      <h1 className="overview">Summary</h1><br></br>
       <div className="results_container">
         <DonutWithText match={match} unmatch={unmatch} />
 
@@ -236,9 +244,24 @@ const onChangeResume = function(event){
           <BarChart score={specificKeywords} />
         </div>
       </div>
-      
-  <div dangerouslySetInnerHTML = {{__html:highlightedText}}></div>  
+
+      <div className="result_table_container">
+        <ResultTable 
+        vitalKeywords={vitalKeywords}
+        resumeRepeatFromPosting={resumeRepeatFromPosting}
+        title={hardSkillTitle} />
+        <ResultTable 
+        vitalKeywords={vitalSoftSkills}
+        resumeRepeatFromPosting={resumeRepeatSoftSkillsPosting}
+        title={softSkillTitle} />
+        <ResultTable 
+        vitalKeywords={vitalKeywords}
+        resumeRepeatFromPosting={resumeRepeatFromPosting} />
+      </div>
+
+      <div dangerouslySetInnerHTML={{ __html: highlightedText }}></div>
     </>
-        
   );
 }
+
+
--- resumex/src/components/ResultTable.js
+++ resumex/src/components/ResultTable.js
@@ -1,11 +1,180 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
-export default function ResultTable(props) {
+
+
+
+
+export default function ResulTable(props) {
+  const useStyles = makeStyles({
+    root: {
+      width: '100%',
+    },
+    container: {
+      maxHeight: 440,
+    },
+  });
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   
+  let vitalKeywords = props.vitalKeywords;
+  let resumeRepeatFromPosting = props.resumeRepeatFromPosting;
+  const title = props.title;
+ 
+
+  const columns = [
+    { id: 'hardskills', label: 'Hard Skills', minWidth: 170 },
+    {
+      id: 'job',
+      label: 'Word Repeat in Job Post',
+      // minWidth: 50,
+      align: 'right',
+      format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+      id: 'resume',
+      label: 'Word Repeat in Job Resume',
+      align: 'right',
+      format: (value) => value.toLocaleString('en-US'),
+    }
+  ];
   
-  return (
-    <div>
+  function createData(hardskills, job, resume) {
+    // const density = population / size;
+    return { hardskills, job, resume };
+  }
+  
+  let rows = [];
+
+
+  if (!vitalKeywords.length) {
+    vitalKeywords = [];
+  }
+
+  if (!resumeRepeatFromPosting.length) {
+    resumeRepeatFromPosting = [];
+  }
+
+  // for(const word of vitalKeywords) {
+  //   for(const word1 of resumeRepeatFromPosting){
+  //     if(vitalKeywords[word][0] === resumeRepeatFromPosting[word1][0]){
+
+  //     }
+  //   }
+  // }
+
+  for (let i = 0; i < vitalKeywords.length; i++) {
+    for (let j = 0; j < resumeRepeatFromPosting.length; j++) {
+    if(resumeRepeatFromPosting[j] === undefined || vitalKeywords[i] === undefined) {
+      resumeRepeatFromPosting[j] = "";
+      vitalKeywords[i] = "";
+    } else {
+      // console.log("not working")
+      // for(const word of vitalKeywords) {
+      //   for(const word1 of resumeRepeatFromPosting){
+        const count = 0; 
+        if (rows.includes(vitalKeywords[i][0])){
+          vitalKeywords[i][1] += 1;
+          if (vitalKeywords[i][0] === resumeRepeatFromPosting[j][0]) {
+            resumeRepeatFromPosting[j][1] += 1;
+          }
+        }
+
+        else {
+          rows.push(createData(vitalKeywords[i][0], vitalKeywords[i][1], count + resumeRepeatFromPosting[j][1]));
+        }
+        
+            
+              
+            console.log("COMING FROM THE vital keywords", vitalKeywords[i]);
+            console.log("coming from resume repeat", resumeRepeatFromPosting[j])
+            console.log("rows", rows)
+          
+            
+          
+          // else {
+          //   rows.push(createData(vitalKeywords[i][0], vitalKeywords[i][1], count));
+          // }
+      //   }
+      // }
+    
       
-    </div>
-  )
+
+    }
+  }
+    
+    // rows.push(createData(vitalKeywords[i][0], vitalKeywords[i][1], resumeRepeatFromPosting[i][1]));
+    // console.log("COMING FROM THE TABLE", vitalKeywords[i]);
+    
+    
+    }
+  
+    
+  return (
+    <Paper className={classes.root}>
+      <h3>{title}</h3>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 }
