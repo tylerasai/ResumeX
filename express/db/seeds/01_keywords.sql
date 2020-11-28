fbb728c53a9ff73d4ca98829f17854f173db1553
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-11-28 13:27:31 -0700

    fixed the getScores function

--- express/db/seeds/01_keywords.sql
+++ express/db/seeds/01_keywords.sql
@@ -532,5 +532,15 @@ values
 ('software development'),
 ('ruby on rails'),
 ('node'),
-('node js')
+('node js'),
+('1 year'),
+('2 years'),
+('3 years'),
+('4 years'),
+('5 years'),
+('6 years'),
+('7 years'),
+('8 years'),
+('9 years'),
+('10 years')
 ;
--- resumex/src/Helpers/getScores.js
+++ resumex/src/Helpers/getScores.js
@@ -1,58 +1,36 @@
 const { createArrayofText } = require("./extrahelp");
-
 //getScores function gives you a score of how many of the vitalKeywords you have on the resume
-
 const array = [["jay", 1] ,["bring bros", 2], ["ho", 1] ,["tyler", 2] ,["bro", 2],["bra",3] ,["bri", 5], ["mom",6], ["dad",6], ["granma",2], ["sher",5], ["adriana lima",4],["brazil", 5]];
 const array1 = [["granma",2], ["sher",5], ["adriana lima",4],["brazil", 5]];
 const text = "adriana lima";
-
 const getScores = function (vitalKeywords, Resume) {
-
   if (!vitalKeywords.length) {
     return [];
   }
-
   const allVital = [];
-  
   //get the top ten vitalKeywords
-  
   for (let i = 0; i < vitalKeywords.length; i++) {
     allVital.push(vitalKeywords[i][0]);
-    
   }
-
   const topTenVital = allVital.slice(0,10);
   console.log("ALL VITAL", allVital);
-  
-  
   //creates an array of words from the text
   const resumeArray = createArrayofText(Resume); 
-  
-  console.log("TOP TEN VITAL",resumeArray);
-  let primaryScore = 0;
-
-
-
+  let scoreArray = [];
   for (let i = 0; i < resumeArray.length; i++) {
-    if (topTenVital.includes(resumeArray[i])) {
-      primaryScore += 1;
+    if (topTenVital.includes(resumeArray[i] + " " + resumeArray[i + 1] + " " + resumeArray[i + 2])) {
+      scoreArray.push(resumeArray[i] + " " + resumeArray[i + 1] + " " + resumeArray[i + 2])
     } else if (topTenVital.includes(resumeArray[i] + " " + resumeArray[i + 1])) {
-      primaryScore += 1;
-    } else if (topTenVital.includes(resumeArray[i] + " " + resumeArray[i + 1] + " " + resumeArray[i + 2])) {
-      primaryScore += 1;
+      scoreArray.push(resumeArray[i] + " " + resumeArray[i + 1]);
+    } else if (topTenVital.includes(resumeArray[i])) {
+      scoreArray.push(resumeArray[i]);
     }
-
   }
-  console.log("TOP TEN VITAL", topTenVital.length);
-  console.log("PRIMARY SCORE", primaryScore);
-
-  if (parseInt((primaryScore/topTenVital.length * 100).toFixed(2)) > 100) {
-    return 100;
-  } else {
-    return parseInt((primaryScore/topTenVital.length * 100).toFixed(2));
-  }
-  
-  
+  console.log("TOP TEN VITAL", topTenVital);
+  console.log("TOP TEN TIVAL LENGTH", topTenVital.length)
+  console.log("SCORE ARRAY", scoreArray);
+  let finalScores = [...new Set(scoreArray)]
+  console.log("FINAL SCORES ARRAY", finalScores)
+  return (finalScores.length/topTenVital.length) * 100
 };
-
 module.exports = getScores;
