7825ef5514ae46124f9abeb444412c05a79af296
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-11-30 10:55:08 -0700

    heroku deployed master verison

--- express/app.js
+++ express/app.js
@@ -14,6 +14,7 @@ const dbHelpers = require('./db/helpers/dbHelpers')(db);
 
 var app = express();
 app.use(cors());
+app.use(express.static("../resumex/build"));
 
 app.use(logger("dev"));
 app.use(express.json());
